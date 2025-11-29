9b41350209935ac37531832cce921cd1972f00e5
CatifiedSWE <smnaveen124@gmail.com>
2025-11-29 11:05:02 +0000

    auto-commit for 7bb24988-f3e2-425f-afe7-c43b94746d8c

--- app/(app)/(slate-group)/template.tsx
+++ app/(app)/(slate-group)/template.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import { BookmarkIcon, HelpCircle, SettingsIcon, UserRound } from "lucide-react";
 
 export default function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
