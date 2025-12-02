3c653744401f94bbbc8f6ec416a4b09b19c72bab
Gohan68 <sharukesh96@gmail.com>
2025-12-02 19:59:00 +0000

    auto-commit for a1bff668-946b-4c07-9059-0433fc27dc6d

--- components/Providers/index.tsx
+++ components/Providers/index.tsx
@@ -10,7 +10,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
     <AuthProvider>
       <ProfileProvider>
         {children}
-        <Toaster richColors />
+        <Toaster />
       </ProfileProvider>
     </AuthProvider>
   );
