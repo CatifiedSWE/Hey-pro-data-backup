7205abaae969d0f5b08e81ff7be001ca6d1ca0c3
Gohan68 <sharukesh96@gmail.com>
2025-12-02 19:59:17 +0000

    auto-commit for 97e522ec-686c-4093-a23e-fcd51cf98d43

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -10,13 +10,18 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
+      toastOptions={{
+        classNames: {
+          toast: "group toast group-[.toaster]:bg-white group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:data-[button]:bg-primary group-[.toast]:data-[button]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:data-[button]:bg-muted group-[.toast]:data-[button]:text-muted-foreground",
+          error: "group-[.toaster]:bg-[#FA6E80] group-[.toaster]:text-white group-[.toaster]:border-none",
+          success: "group-[.toaster]:bg-[#31A7AC] group-[.toaster]:text-white group-[.toaster]:border-none",
+        },
+      }}
       {...props}
     />
   )
