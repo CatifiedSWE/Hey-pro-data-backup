36728d72e1a148b5c2fd3f77767c0318e448dd63
Gohan68 <sharukesh96@gmail.com>
2025-12-03 08:20:43 +0000

    auto-commit for 9d641f3f-4535-45b0-8fba-19f82dad063f

--- app/(app)/profile/components/highlights-text.tsx
+++ app/(app)/profile/components/highlights-text.tsx
@@ -1,17 +1,5 @@
 import * as React from "react";
 
-const palette = [
-  "#FA6E80",
-  "#F2748E",
-  "#D47AA8",
-  "#A584BF",
-  "#7F8FC7",
-  "#6BA1C6",
-  "#5FB5C4",
-  "#56C1BE",
-  "#31A7AC",
-];
-
 const text = "HEYLIGHTS";
 
 type HighlightsTextProps = {
@@ -21,18 +9,32 @@ type HighlightsTextProps = {
 
 export default function HighlightsText({
   className = "",
-  letterClassName = "text-[50px] font-[700] leading-none",
+  letterClassName = "",
 }: HighlightsTextProps) {
   return (
     <div
-      className={`[writing-mode:vertical-rl] rotate-180 ${className}`}
-      style={{ lineHeight: "1" }}
+      className={`flex flex-col items-center justify-start ${className}`}
+      style={{ 
+        width: '33px',
+        gap: '7px'
+      }}
     >
       {text.split("").map((char, i) => (
         <span
           key={i}
-          className={letterClassName}
-          style={{ color: palette[i % palette.length] }}
+          className="uppercase"
+          style={{
+            fontFamily: 'Poppins, sans-serif',
+            fontWeight: 700,
+            fontSize: '22px',
+            lineHeight: '100%',
+            letterSpacing: '0%',
+            textAlign: 'center',
+            background: 'linear-gradient(90deg, #FA6E80 0%, #6A89BE 41.52%, #85AAB7 62.27%, #31A7AC 103.79%)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            backgroundClip: 'text',
+          }}
         >
           {char}
         </span>
