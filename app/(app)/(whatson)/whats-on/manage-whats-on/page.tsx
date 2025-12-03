b3586736cb77da68b7bfb3a06d480d4b8be2211b
Gohan68 <sharukesh96@gmail.com>
2025-12-03 01:39:30 +0000

    auto-commit for 2fba0b88-aeac-49c9-9ee9-f7dc822520e6

--- app/(app)/(whatson)/whats-on/manage-whats-on/page.tsx
+++ app/(app)/(whatson)/whats-on/manage-whats-on/page.tsx
@@ -287,12 +287,6 @@ export default function ManageWhatsOnPage() {
             {events.length === 0 && !loading && (
                 <div className="text-center py-12">
                     <p className="text-gray-500 text-lg mb-4">You haven&apos;t created any events yet</p>
-                    <Link
-                        href="/whats-on/manage-whats-on/add-new"
-                        className="inline-block bg-[#31A7AC] text-white px-6 py-3 rounded-full hover:bg-[#288a8e] transition-colors"
-                    >
-                        Create Your First Event
-                    </Link>
                 </div>
             )}
 
