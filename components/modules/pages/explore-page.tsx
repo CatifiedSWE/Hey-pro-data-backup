e6fe8e30435462da8a437da0c2b749c7a290671a
CatifiedSWE <smnaveen124@gmail.com>
2025-12-05 17:17:03 +0000

    auto-commit for a9e7a652-e717-4ab0-9381-915ee6f51225

--- components/modules/pages/explore-page.tsx
+++ components/modules/pages/explore-page.tsx
@@ -123,9 +123,9 @@ export default function ExplorePage({
               </div>
 
               {/* Main Content Container */}
-              <div className="w-full max-w-6xl mx-auto">
+              <div className="w-full max-w-7xl mx-auto">
                 {/* Header Info Section */}
-                <div className="flex flex-col gap-4 pt-16 sm:pt-20 px-4 sm:px-8">
+                <div className="flex flex-col gap-4 pt-14 sm:pt-20 px-4 sm:px-12">
                   <div className="space-y-3">
                     <DialogHeader className="p-0 space-y-1 text-left">
                       <DialogTitle className="text-2xl sm:text-3xl md:text-4xl font-semibold text-black text-left">
