a90b01b074acc1469b794981491fb76aec28f663
CatifiedSWE <smnaveen124@gmail.com>
2025-12-06 10:29:35 +0000

    creww fix

--- components/modules/pages/explore-page.tsx
+++ components/modules/pages/explore-page.tsx
@@ -18,7 +18,7 @@ export default function ExplorePage({
 
   return (
     <div className="w-full overflow-x-hidden">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
         {projectsCardData.length > 0 ? (
           projectsCardData.map((project) => (
             <ProjectCard 
